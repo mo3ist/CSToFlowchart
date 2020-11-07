262fdaefbf4b2b909f4c6e086f59762d19b8ce2d
mo3ist <50373064+mo3ist@users.noreply.github.com>
2020-11-07 04:56:04 +0200

    Added if/else if/else support

--- mkchr.py
+++ mkchr.py
@@ -1,10 +1,9 @@
 from graphviz import Digraph
 import sys
 
-# TODO: statement recognition : differentiate if from else if from else...
-# TODO: handle else if and else. 
-# TODO: Make conditionals point to what's next to them.
-# TODO: Handle for loops.
+# TODO: Handle loops.
+# TODO: clean the code [comments, vars, funcs..]
+# BUG: the last line in the last block in a 'rel if' chunk that is before another if statement isn't connected to the next line
 
 def main(code_file, export_file):
     # open the file
@@ -13,7 +12,7 @@ def main(code_file, export_file):
 
     # get the graph
     graph = []
-    get_graph(0, [], code, graph)
+    get_graph(0, [], code, graph, [])
     
     # visualize the graph
     dot = Digraph()
@@ -23,7 +22,7 @@ def main(code_file, export_file):
     
     dot.render(export_file, view=True)
 
-def get_graph(index, branch, code, graph):
+def get_graph(index, branch, code, graph, if_tracker):
     """
     Recursively converts the nested code to a graph
     """
@@ -33,8 +32,8 @@ def get_graph(index, branch, code, graph):
 
     # terminate recurtion instance with a return value
     if "}" in code[index]:
-        ## link the next line to the pervious neighbouring parent node
-        # the line which represents the neighbour is one line brefore the branching point
+        ## link the neighbouring socpes
+        # neighbour is previous branch point
         neighbouring_node = branch[-1]-1
         next_index = index+1
 
@@ -43,20 +42,81 @@ def get_graph(index, branch, code, graph):
             if "}" not in code[next_index]:
                 graph.append([neighbouring_node, next_index])
 
+        ## connect the true branch of an if statement to the last if block 
+        if index < len(code)-1: # not the last line
+
+            # add the last line in the statement to the tracker 
+            if if_tracker:
+                ## if the current branch was an if statement
+                # NOTE: potential index error
+                if code[(branch[-1]-1)].strip().split()[0] in ["if", "else"]:
+                    # add the previous line to if_tracker [end of 'true' branch]
+                    if_tracker[-1].append(index - 1) 
+                    
+            # if last block
+            next_line = code[index+1].strip().split()
+            # no else's or if else's 
+            if not ("else" in next_line or ("if" in next_line and ("else" in next_line))):
+                # connect lines 
+                for block in if_tracker:
+                    graph.append([block[1], index+1])
+
         ## return the current line to set the parent method's local index
         return index
 
     # branch to a new instance 
     elif "{" in code[index]:   
-        index = get_graph(index+1, branch+[index], code, graph)
-    
+
+        # We're in a new branch
+        
+        # NOTE: Potential index error 
+
+        ## handle if statements
+        ## since if's are related => (if, else if, else if, ..., else)
+        ## in each block, if true, branch then go to after else' block
+        ##            and if it's false then go to the next block
+        ## so talking in terms of the algo here, what should be done
+        ## is that we keep track of related if's then point 'true'
+        ## post else' block 
+
+        current_line = code[index-1].strip().split()
+
+        # if
+        if "if" in current_line and not "else" in current_line:
+            
+            # connect the related if's blocks to the graph [the head 'if' to whatever is the prev line]
+            if len(graph) > 2:
+                # NOTE: since the delimiter is "{" instead of the keyword itself, 
+                # the "if" line is alone in the graph as well as its previous line
+                # so the line before "if" is graph[-2]
+                graph[-2].append(index-1)
+        
+            # clear the tracker
+            if_tracker = []
+
+            # add a new rel if statement
+            if_tracker.append([index-1])  
+
+        # elif
+        elif "else" in current_line and "if" in current_line :
+            # add a new rel if statement
+            if_tracker.append([index-1])  
+        # else
+        elif 'else' in current_line and 'if' not in current_line:
+            # add a new rel if statement
+            if_tracker.append([index-1])  
+        branch += [index]
+        index = get_graph(index+1, branch, code, graph, if_tracker)
     else:
         # connect the node to the previous & make a space for the next
         
         if graph:
             # connect
             if len(graph[-1]) < 2:
-                graph[-1].append(index)
+                # check that the node we'll conenct to is a normal line IN THE SCOPE
+                
+                if "{" not in code[index + 1]:
+                    graph[-1].append(index)
 
             # add
             graph.append([index])
@@ -67,11 +127,11 @@ def get_graph(index, branch, code, graph):
 
     ## will execute after branching
     # walk in a sequence    
-    index = get_graph(index+1, branch, code, graph)
+    index = get_graph(index+1, branch, code, graph, if_tracker)
     # return the current line to set the parent method's local [i]
     return index
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     if len(sys.argv) == 3:
         code_file = sys.argv[1]
         export_file = sys.argv[2]
